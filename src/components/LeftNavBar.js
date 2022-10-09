43a496576781cc21ffbd5608b6d19a9f71b2c62d
Suraj10061997 <surajsingh10061997@gmail.com>
2022-10-09 21:13:21 +0530

    deploying laundry cart-app

--- src/components/LeftNavBar.js
+++ src/components/LeftNavBar.js
@@ -5,8 +5,8 @@ const LeftNavBar = () => {
   return (
     <>
       <nav id="leftNav">
-        <img className="navIcon" src="/images/home-run.svg" />
-        <img className="navIcon" src="/images/more.svg" />
+        <img className="navIcon" src="/images/home-run.svg" alt="home-run-image" />
+        <img className="navIcon" src="/images/more.svg" alt="more-image"/>
         <div id="listIconCover">
           <img className="navIcon" id="listIcon" src="/images/list.svg" alt="list_image"/>
         </div>
